9eee498d8d945bbe408e9fd5a20fbd4e89e38b5e
fiyinfoluwa <fiyinfoluwa129@gmail.com>
2025-12-11 17:19:24 +0100

    message

---
+++ script.js
@@ -0,0 +1,31 @@
+let carlight = prompt("enter the traffic light and i will tell you what to do")
+
+if(carlight === "red"){
+console.log("stop")
+}else if(carlight == "yellow"){
+    console.log("get ready")
+}else if(carlight == "green"){
+    console.log("go")
+}else{
+    console.log("invalid")
+}
+
+let passenger = prompt("tell me car color so as to move")
+
+if(carlight === "red"){
+    console.log("go")
+}else if(carlight === "green"){
+    console.log("stop")
+}else if(carlight === "yellow"){
+    console.log("get ready")
+}else{
+    console.log("invalid")
+}
+
+
+
+
+
+
+
+
